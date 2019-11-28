bdf02b2d088e8ca2e7bcc178a7b291ba2d1e9160
rodratajczak-avalara <rodratajczak-avalara@users.noreply.github.com>
2019-11-27 18:27:06 -0600

    updated to use shared dll's for common data definitons

--- AddFormCellDependenciesFromExcel/AddFormCellDependenciesFromExcel/Program.cs
+++ AddFormCellDependenciesFromExcel/AddFormCellDependenciesFromExcel/Program.cs
@@ -9,6 +9,7 @@ using DocumentFormat.OpenXml.Spreadsheet;
 using System.IO;
 using System.Net;
 using Newtonsoft.Json;
+using TaxFormCatalog.Shared.Entities;
 
 namespace AddFormCellDependenciesFromExcel
 {
@@ -25,7 +26,6 @@ namespace AddFormCellDependenciesFromExcel
         /// <remarks>args[4] = form master id to attach formCellDependencies</remarks>
         static void Main(string[] args)
         {
-            List<InputData> dependencies = new List<InputData>(); // = null;
             List<InputData2> newDependencies = new List<InputData2>(); // = null;
 
             long updateFormMasterId = long.TryParse(args[4], out updateFormMasterId) ? updateFormMasterId : -1;
@@ -63,7 +63,6 @@ namespace AddFormCellDependenciesFromExcel
 
                         foreach (Row row in sheetData.Elements<Row>())
                         {
-                            InputData dependency = new InputData();
                             InputData2 newDependency = new InputData2();
                             int rowcolumn = 1;
                             int tempMonths = -1;
@@ -74,34 +73,17 @@ namespace AddFormCellDependenciesFromExcel
 
                                 switch (rowcolumn)
                                 {
-                                    /*
-                                    case 1: dependency.TaxFormCode = ReadExcelCell(c, workbookPart); break;
-                                    case 2: dependency.CurrentPeriod = ReadExcelCell(c, workbookPart); break;
-                                    case 3: dependency.Months = int.TryParse(ReadExcelCell(c, workbookPart), out tempMonths) ? tempMonths : 0; break;
-                                    case 4: dependency.OriginCellName = ReadExcelCell(c, workbookPart); break;
-                                    case 5: dependency.DestinationCellName = ReadExcelCell(c, workbookPart); break;
-                                    case 6: dependency.CityCountyCode = ReadExcelCell(c, workbookPart); break;
-                                    case 7: dependency.TaxType = ReadExcelCell(c, workbookPart); break;
-                                    case 8: dependency.Status = ReadExcelCell(c, workbookPart); break;
-                                    case 9: dependency.Notes = ReadExcelCell(c, workbookPart); break;
-                                    */
                                     //New Format
                                     case 1: newDependency.TaxFormCode = ReadExcelCell(c, workbookPart); break;
                                     case 2: newDependency.SummaryLabel = ReadExcelCell(c, workbookPart); break;
                                     case 3: newDependency.FieldName = ReadExcelCell(c, workbookPart) ?? null; break;
                                     case 4: newDependency.CurrentPeriod = ReadExcelCell(c, workbookPart) ?? null; break;
                                     case 5: newDependency.Months = int.TryParse(ReadExcelCell(c, workbookPart), out tempMonths) ? tempMonths : 0; break;
-                                    case 6: newDependency.Status = ReadExcelCell(c, workbookPart); break;
+                                    case 6: newDependency.PriorYear = ReadExcelCell(c, workbookPart); break;
                                 }
 
                                 rowcolumn++;
                             }
-                            /*
-                            if (dependency.TaxFormCode != "Form")
-                            {
-                                dependencies.Add(dependency);
-                            }
-                            */
 
                             if (newDependency.TaxFormCode != "Form")
                             {
@@ -112,7 +94,7 @@ namespace AddFormCellDependenciesFromExcel
                 }
 
                 List<FormCellDependency> recordsToAdd = new List<FormCellDependency>();
-                int totalRecords = dependencies.Count;
+                int totalRecords = newDependencies.Count;
                 string prevTaxFormCode = string.Empty;
                 long formMasterId = 0;
                 List<FormCellDependency> existingDependencies = GetFormCellDependencies(updateFormMasterId, args[1], args[2], args[3]);
@@ -121,7 +103,6 @@ namespace AddFormCellDependenciesFromExcel
 
 
                 // Create List of FormCellDependency records to add 
-                //foreach (InputData item in dependencies)
                 foreach (InputData2 item in newDependencies)
                 {
                     if (!item.TaxFormCode.Equals(prevTaxFormCode))
@@ -143,8 +124,8 @@ namespace AddFormCellDependenciesFromExcel
                         try
                         {
                             FormCellDependency dependency = null;
-                            var annotation = formAnnotations.Any() ? formAnnotations.Where(a => a.AnnotationName == item.FieldName).FirstOrDefault() : null;
-                            var summaryLabel = summaryLabels.Any() ? summaryLabels.Where(a => a.SummaryLabelCode == item.SummaryLabel).FirstOrDefault() : null;
+                            Annotation annotation = formAnnotations.Any() ? formAnnotations.Where(a => a.AnnotationName == item.FieldName).FirstOrDefault() : null;
+                            SummaryLabel summaryLabel = summaryLabels.Any() ? summaryLabels.Where(a => a.SummaryLabelCode == item.SummaryLabel || a.Description == item.SummaryLabel).FirstOrDefault() : null;
                             if (annotation == null)
                             {
                                 if (summaryLabel == null)
@@ -173,11 +154,11 @@ namespace AddFormCellDependenciesFromExcel
                                 dependencyToAdd.FormCellDependencyId = dependency == null ? -1 : dependency.FormCellDependencyId;
                                 dependencyToAdd.FormMasterId = updateFormMasterId;
                                 dependencyToAdd.FormMasterOptionId = formMasterId;
-                                dependencyToAdd.AnnotationId = annotation == null ? null : annotation.AnnotationId;
-                                dependencyToAdd.SummaryLabelId = summaryLabel == null ? null : summaryLabel.SummaryLabelId; //null;  // needs to be added if we plan to use this for other forms
-                                dependencyToAdd.IncludeCurrentPeriod = item.CurrentPeriod.ToUpper() == "YES" ? true : false;
+                                dependencyToAdd.AnnotationId = annotation == null ? null : (long?)annotation.AnnotationId;
+                                dependencyToAdd.SummaryLabelId = summaryLabel == null ? null : (long?)summaryLabel.SummaryLabelId; 
+                                dependencyToAdd.IncludeCurrentPeriod = item.CurrentPeriod.ToUpper() == "1" ? true : false;
                                 dependencyToAdd.MonthsAgo = item.Months;
-                                dependencyToAdd.PriorYearEndFiscal = false;  // needs to be added if we plan to use this for other forms
+                                dependencyToAdd.PriorYearEndFiscal = item.PriorYear.ToUpper() == "1" ? true : false;  
                                 dependencyToAdd.CreatedDate = System.DateTime.UtcNow;
                                 dependencyToAdd.CreatedUserId = 0;
                                 dependencyToAdd.ModifiedDate = System.DateTime.UtcNow;
@@ -502,19 +483,6 @@ namespace AddFormCellDependenciesFromExcel
 
     }
 
-    public class InputData
-    {
-        public String TaxFormCode { get; set; }
-        public String CurrentPeriod { get; set; }
-        public int Months { get; set; }
-        public String OriginCellName { get; set; }
-        public String DestinationCellName { get; set; }
-        public String CityCountyCode { get; set; }
-        public String TaxType { get; set; }
-        public String Status { get; set; }
-        public String Notes { get; set; }
-    }
-
     public class InputData2
     {
         public String TaxFormCode { get; set; }
@@ -522,7 +490,7 @@ namespace AddFormCellDependenciesFromExcel
         public String FieldName { get; set; }
         public String CurrentPeriod { get; set; }
         public int Months { get; set; }
-        public String Status { get; set; }
+        public String PriorYear { get; set; }
     }
 
     public class DeleteFormCellDependency
@@ -532,196 +500,4 @@ namespace AddFormCellDependenciesFromExcel
         public Boolean Deleted { get; set; }
     }
 
-    public class FormCellDependency
-    {
-        public Int64 FormCellDependencyId { get; set; }
-        public Int64 FormMasterId { get; set; }
-        public Int64? SummaryLabelId { get; set; }
-        public Int64 FormMasterOptionId { get; set; }
-        public Boolean IncludeCurrentPeriod { get; set; }
-        public Int32 MonthsAgo { get; set; }
-        public Int64 CreatedUserId { get; set; }
-        public DateTime CreatedDate { get; set; }
-        public Int64 ModifiedUserId { get; set; }
-        public DateTime ModifiedDate { get; set; }
-        public Boolean PriorYearEndFiscal { get; set; }
-        public Int64? AnnotationId { get; set; }
-    }
-
-
-    public partial class FormMaster
-    {
-        public Int64 FormMasterId { get; set; }
-        public Int64 FormTypeId { get; set; }
-        public String TaxFormCode { get; set; }
-        public String LegacyReturnName { get; set; }
-        public String TaxFormName { get; set; }
-        public String Description { get; set; }
-        public Boolean IsEffective { get; set; }
-        public String Country { get; set; }
-        public String Region { get; set; }
-        public String AuthorityName { get; set; }
-        public String ShortCode { get; set; }
-        public Int16? DueDay { get; set; }
-        public Int16? DelinquentDay { get; set; }
-        public Byte? FiscalYearStartMonth { get; set; }
-        public Boolean HasMultiFrequencies { get; set; }
-        public Boolean IsPOARequired { get; set; }
-        public Boolean IsRegistrationRequired { get; set; }
-        public Boolean HasMultiRegistrationMethods { get; set; }
-        public Boolean HasSchedules { get; set; }
-        public Boolean HasMultiFilingMethods { get; set; }
-        public Boolean HasMultiPayMethods { get; set; }
-        public Boolean IsEFTRequired { get; set; }
-        public Boolean IsFilePayMethodLinked { get; set; }
-        public Int64 MailingReceivedRuleId { get; set; }
-        public Int64 ProofOfMailingId { get; set; }
-        public Boolean IsNegAmountAllowed { get; set; }
-        public Boolean AllowNegativeOverallTax { get; set; }
-        public Boolean IsNettingRequired { get; set; }
-        public Int64 RoundingMethodId { get; set; }
-        public Decimal VendorDiscountAnnualMax { get; set; }
-        public Boolean VersionsRequireAuthorityApproval { get; set; }
-        public Int64 OutletReportingMethodId { get; set; }
-        public Boolean HasReportingCodes { get; set; }
-        public Boolean HasPrepayments { get; set; }
-        public Boolean GrossIncludesInterstateSales { get; set; }
-        public String GrossIncludesTax { get; set; }
-        public Boolean HasEfileFee { get; set; }
-        public Boolean HasEpayFee { get; set; }
-        public Boolean HasDependencies { get; set; }
-        public String RequiredEfileTrigger { get; set; }
-        public String RequiredEftTrigger { get; set; }
-        public Boolean VendorDiscountEfile { get; set; }
-        public Boolean VendorDiscountPaper { get; set; }
-        public Boolean? PeerReviewed { get; set; }
-        public Int64? PeerReviewedId { get; set; }
-        public DateTime? PeerReviewedDate { get; set; }
-        public Int64 CreatedUserId { get; set; }
-        public DateTime CreatedDate { get; set; }
-        public Int64 ModifiedUserId { get; set; }
-        public DateTime ModifiedDate { get; set; }
-        public String DORAddressMailTo { get; set; }
-        public String DORAddress1 { get; set; }
-        public String DORAddress2 { get; set; }
-        public String DORAddressCity { get; set; }
-        public String DORAddressRegion { get; set; }
-        public String DORAddressPostalCode { get; set; }
-        public String DORAddressCountry { get; set; }
-        public String ZeroAddressMailTo { get; set; }
-        public String ZeroAddress1 { get; set; }
-        public String ZeroAddress2 { get; set; }
-        public String ZeroAddressCity { get; set; }
-        public String ZeroAddressRegion { get; set; }
-        public String ZeroAddressPostalCode { get; set; }
-        public String ZeroAddressCountry { get; set; }
-        public String AmendedAddressMailTo { get; set; }
-        public String AmendedAddress1 { get; set; }
-        public String AmendedAddress2 { get; set; }
-        public String AmendedAddressCity { get; set; }
-        public String AmendedAddressRegion { get; set; }
-        public String AmendedAddressPostalCode { get; set; }
-        public String AmendedAddressCountry { get; set; }
-        public Boolean OnlineBackFiling { get; set; }
-        public Boolean OnlineAmendedReturns { get; set; }
-        public String PrepaymentFrequency { get; set; }
-        public Boolean? OutletLocationIdentifiersRequired { get; set; }
-        public String ListingSortOrder { get; set; }
-        public String DORWebsite { get; set; }
-        public Boolean FileForAllOutlets { get; set; }
-        public Boolean PaperFormsDoNotHaveDiscounts { get; set; }
-        public Boolean StackAggregation { get; set; }
-        public Byte? RoundingPrecision { get; set; }
-        public Decimal? InconsistencyTolerance { get; set; }
-        public DateTime EffDate { get; set; }
-        public DateTime EndDate { get; set; }
-        public Boolean VisibleToCustomers { get; set; }
-        public Boolean RequiresOutletSetup { get; set; }
-        public Boolean AchCreditAllowed { get; set; }
-        public String ReportLevel { get; set; }
-        public Boolean PostOfficeValidated { get; set; }
-        public String StackAggregationOption { get; set; }
-        public String SstBehavior { get; set; }
-        public String NonSstBehavior { get; set; }
-        public String DORPhoneNumber { get; set; }
-        public Int64? AverageCheckClearDays { get; set; }
-        public Boolean FilterZeroRatedLineDetails { get; set; }
-        public Boolean AllowsBulkFilingAccounts { get; set; }
-        public String BulkAccountInstructionLink { get; set; }
-        public String RegistrationIdFormat { get; set; }
-        public String ThresholdTrigger { get; set; }
-        public String TransactionSortingOption { get; set; }
-        public Int64? ContentReviewFrequencyId { get; set; }
-        public Int64? AliasForFormMasterId { get; set; }
-        public Int32 DefaultPrepayPercentage { get; set; }
-        public Boolean AllowFixedPrepayAmount { get; set; }
-        public Int64 BatchedEnvelopeRuleId { get; set; }
-        public Int64? FormLogicTypeId { get; set; }
-    }
-
-
-    public partial class Annotation
-    {
-        public Int64? AnnotationId { get; set; }
-        public Int64 FormVersionId { get; set; }
-        public String AnnotationName { get; set; }
-        public Boolean ReadOnly { get; set; }
-        public String JsFormat { get; set; }
-        public Int64? ImportPathId { get; set; }
-        public String FullName { get; set; }
-        public String Calculation { get; set; }
-        public String Validation { get; set; }
-        public String FormatView { get; set; }
-        public String Description { get; set; }
-        public String DefaultValue { get; set; }
-        public Boolean EmptyIfZero { get; set; }
-        public String CommonName { get; set; }
-        public Int64 CreatedUserId { get; set; }
-        public DateTime CreatedDate { get; set; }
-        public Int64 ModifiedUserId { get; set; }
-        public DateTime ModifiedDate { get; set; }
-        public String GroupCode { get; set; }
-        public String ReportLevel { get; set; }
-        public String DataType { get; set; }
-        public Boolean? PdfFormatPercent { get; set; }
-        public Int64? PdfFormatDecimalPlaces { get; set; }
-        public Boolean? PdfFormatUseSeparator { get; set; }
-        public String PdfFormatDateMask { get; set; }
-        public String TaxTypeIds { get; set; }
-        public String RateTypeIds { get; set; }
-        public String DeductionTypeIds { get; set; }
-        public Int64? SummaryLabelId { get; set; }
-        public String JurisName { get; set; }
-        public Boolean IsHidden { get; set; }
-        public Int64? PositionHeaderId { get; set; }
-        public String NegativeFormat { get; set; }
-        public Boolean RemoveDecimal { get; set; }
-        public Boolean NegateTaxTypes { get; set; }
-        public Boolean NegateRateTypes { get; set; }
-        public Boolean NegateDeductionTypes { get; set; }
-        public Boolean NegateGroupCode { get; set; }
-        public Boolean NegateJurisName { get; set; }
-        public Boolean NegateReportLevel { get; set; }
-        public Boolean HiddenFromCompliance { get; set; }
-        public String JsonData { get; set; }
-        public String ValidationSeverity { get; set; }
-        public String ValidationMessage { get; set; }
-        public Int64 PdfFormatDecimal { get; set; }
-        public Int64 PdfFormatSeparator { get; set; }
-        public Boolean Recalculate { get; set; }
-        public String Instruction { get; set; }
-        public Int32? MaxLength { get; set; }
-    }
-
-    public partial class SummaryLabel
-    {
-        public Int64? SummaryLabelId { get; set; }
-        public String SummaryLabelCode { get; set; }
-        public String Description {get;set;}
-        public Int64 CreatedUserId { get; set; }
-        public DateTime CreatedDate { get; set; }
-        public Int64 ModifiedUserId { get; set; }
-        public DateTime ModifiedDate { get; set; }
-        public Boolean IsNumeric { get; set; }
-    }
 }
